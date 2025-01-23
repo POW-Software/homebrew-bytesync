cfc5bf17edba00a52c75448631f08ce440c70833
Paul Fresquet <paul.fresquet@pow-software.com>
2025-01-23 07:16:33 +0100

    feat: update linux formula

--- Casks/bytesync.rb
+++ Casks/bytesync.rb
@@ -1,12 +1,12 @@
 cask "bytesync" do
     version "2025.1.49"
-    sha256 "E628FC9D0B6163A97F578D1C20F46D2F4BB85EFFE7AC7D10523F6C4E87620F47"
-  
+    
     url "https://powbytesyncupdates.blob.core.windows.net/updates/ByteSync_osx-x64_#{version}.app.zip"
+    sha256 "E628FC9D0B6163A97F578D1C20F46D2F4BB85EFFE7AC7D10523F6C4E87620F47"
+
     name "ByteSync"
-    desc "Synchronisation de fichiers rapide et sécurisée"
+    desc "Fast & secure file synchronization"
     homepage "https://www.bytesyncapp.com"
-    # license "MIT"
   
     app "ByteSync.app"
   end
