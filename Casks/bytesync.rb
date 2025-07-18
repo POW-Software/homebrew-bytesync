d82c9503a2c839d7bb90d8e5815a708385daca0a
Paul Fresquet <61119222+paul-fresquet@users.noreply.github.com>
2025-07-18 16:38:08 +0200

    feat: update to version 2025.7.2 (#16)

--- Casks/bytesync.rb
+++ Casks/bytesync.rb
@@ -1,8 +1,8 @@
 cask "bytesync" do
-    version "2025.6.6"
+    version "2025.7.2"
     
     url "https://powbytesyncupdates.blob.core.windows.net/updates/ByteSync_osx-x64_#{version}.app.zip"
-    sha256 "ad869b9066984ccf944a39e3fe3e01aa373367751888aa50759219a19501d39f"
+    sha256 "dcf1753624df0d1433c2e5e8ebc4734d1751f2ccc84affc031cf93cf00dc0e81"
 
     name "ByteSync"
     desc "Fast & secure file synchronization"
