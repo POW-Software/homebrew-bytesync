e378b96fbf2dcea4890274781b90392344c16540
Paul Fresquet <paul.fresquet@pow-software.com>
2025-09-18 12:46:10 +0200

    feat: update to version 2025.9.4

--- Casks/bytesync.rb
+++ Casks/bytesync.rb
@@ -1,8 +1,8 @@
 cask "bytesync" do
-    version "2025.9.3"
+    version "2025.9.4"
     
     url "https://powbytesyncupdates.blob.core.windows.net/updates/ByteSync_osx-x64_#{version}.app.zip"
-    sha256 "cd1aa57640ed427f360e90bdf23f80980b7b35105294c6907f45c5bc1642f86b"
+    sha256 "ecb76b9917e861e51475c2c25ddc4f0d8b66704b9e07e3dad5f406752b77cf5c"
 
     name "ByteSync"
     desc "Fast & secure file synchronization"
